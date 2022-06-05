d779acf753ef7cd89d8c347c6b1ac30991306c24
okita_kamegoro <keisi1100525@outlook.jp>
2022-06-05 15:14:22 +0900

    fish の設定を修正

--- modules/fish/conf.d/mysql.fish
+++
@@ -1,6 +0,0 @@
-if type -q mysql
-    # for mysql@5.7
-    set -gx LDFLAGS "-L/usr/local/opt/mysql@5.7/lib"
-    set -gx CPPFLAGS "-I/usr/local/opt/mysql@5.7/include"
-    set -g fish_user_paths "/usr/local/opt/mysql@5.7/bin" $fish_user_paths
-end
--- modules/fish/conf.d/python.fish
+++
@@ -1,4 +0,0 @@
-# for python
-if type -q python
-    fish_add_path /usr/local/opt/python@3.9/libexec/bin
-end

72e8dd634c1c7ea20a0bafc27c02791ce71c7a00
okita_kamegoro <keisi1100525@outlook.jp>
2023-09-02 10:10:52 +0900

    add pyenv

--- modules/fish/conf.d/python.fish
+++ modules/fish/conf.d/python.fish
@@ -1,4 +1,13 @@
-# for python
-if type -q python3
-    alias python="python3"
+# for pyenv
+if type -q pyenv
+    # Add pyenv executable to PATH by running
+    # the following interactively:
+
+    set -Ux PYENV_ROOT $HOME/.pyenv
+    set -U fish_user_paths $PYENV_ROOT/shims $fish_user_paths
+
+    # Load pyenv automatically by appending
+    # the following to ~/.config/fish/config.fish:
+
+    pyenv init - | source
 end
